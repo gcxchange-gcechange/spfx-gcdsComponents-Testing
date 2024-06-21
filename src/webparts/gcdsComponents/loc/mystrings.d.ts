2b5cdc3e6ba482f8ad5a7661f4b329e42d927474
gabmor38 <gabriela.morenor@gmail.com>
2024-06-21 08:38:50 -0400

    initial commit

---
+++ src/webparts/gcdsComponents/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IGcdsComponentsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'GcdsComponentsWebPartStrings' {
+  const strings: IGcdsComponentsWebPartStrings;
+  export = strings;
+}
