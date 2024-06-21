5bc7411d5fdc70cfb848a7932bc1ad715468ca91
gabmor38 <gabriela.morenor@gmail.com>
2024-06-21 08:43:59 -0400

    added fontawesome cdnjs api link to head

--- src/webparts/gcdsComponents/GcdsComponentsWebPart.ts
+++ src/webparts/gcdsComponents/GcdsComponentsWebPart.ts
@@ -37,6 +37,11 @@ export default class GcdsComponentsWebPart extends BaseClientSideWebPart<IGcdsCo
   }
 
   protected onInit(): Promise<void> {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css';
+    link.crossOrigin = 'anonymous';
+    document.head.appendChild(link);
     return this._getEnvironmentMessage().then(message => {
       this._environmentMessage = message;
     });
